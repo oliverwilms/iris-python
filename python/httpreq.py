bb63e3088908396cc0655ece2436104f5d6ef004
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-04-28 21:37:57 -0500

    Update httpreq.py

--- python/httpreq.py
+++ python/httpreq.py
@@ -25,4 +25,5 @@ for line in r.iter_lines():
     # filter out keep-alive new lines
     if line:
         decoded_line = line.decode('utf-8')
-        print(json.loads(decoded_line))
+        y = (json.loads(decoded_line))
+        print(y["message"])
