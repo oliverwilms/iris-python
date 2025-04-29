ce1702bc365a7436928edadd01324f2fb93d7808
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-04-28 21:20:33 -0500

    Update httpreq.py

--- python/httpreq.py
+++ python/httpreq.py
@@ -1,8 +1,27 @@
 import json
-import requests
+from requests import Request, Session
 
 url = "http://localhost:11435/api/chat"
-r = requests.get('http://localhost:11435/api/chat', stream=True)
+#r = requests.get('http://localhost:11435/api/chat', stream=True)
+
+s = Session()
+
+req = Request('POST', url, data=data, headers=headers)
+prepped = req.prepare()
+
+# do something with prepped.body
+prepped.body = '{"model": "llama3.2", "messages": [{"role": "system", "content": "$question"}, {"role": "user", "content": "$prompt"}]}'
+
+# do something with prepped.headers
+del prepped.headers['Content-Type']
+
+r = s.send(prepped,
+    stream=stream,
+    verify=verify,
+    proxies=proxies,
+    cert=cert,
+    timeout=timeout
+)
 
 for line in r.iter_lines():
 
