ee74c1bad49fea6b552b9e7a0cccf9335874d8de
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-05-06 20:23:21 -0500

    Create testreq.py

---
+++ python/testreq.py
@@ -0,0 +1,36 @@
+import json
+from requests import Request, Session
+
+url = "http://localhost:53795/api/test/Service"
+data = '{"model": "llama3.2", "messages": [{"role": "system", "content": "$question"}, {"role": "user", "content": "$prompt"}]}'
+
+s = Session()
+
+req = Request('POST', url, data=data,)
+#prepped = req.prepare()
+prepped = s.prepare_request(req)
+
+# Merge environment settings into session
+settings = s.merge_environment_settings(prepped.url, {}, None, None, None)
+
+# do something with prepped.body
+prepped.body = '{"model": "llama3.2", "messages": [{"role": "system", "content": "$question"}, {"role": "user", "content": "$prompt"}]}'
+
+# do something with prepped.headers
+#del prepped.headers['Content-Type']
+r = s.send(prepped, **settings)
+
+answer = []
+
+for line in r.iter_lines():
+
+    # filter out keep-alive new lines
+    if line:
+        decoded_line = line.decode('utf-8')
+        y = (json.loads(decoded_line))
+        message = y["message"]
+        content = message["content"]
+        answer.append(content)
+
+print(str(answer))
+
