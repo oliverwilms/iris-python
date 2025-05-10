1a07d4e35dc44a8a80884b8f3185f56b50460fa1
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-05-09 21:56:24 -0500

    url = "http://localhost:11435/api/chat"

--- python/testreq.py
+++ python/testreq.py
@@ -24,7 +24,7 @@ data1 = data.replace("$question",question_data)
 data2 = data1.replace("$prompt",prompt_data)
 print(data2)
 s = Session()
-# url = "http://localhost:11435/api/chat"
+url = "http://localhost:11435/api/chat"
 req = Request('POST', url, data=data2,)
 #prepped = req.prepare()
 prepped = s.prepare_request(req)
