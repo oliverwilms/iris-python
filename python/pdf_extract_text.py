6c9f79958743c0ababfa9ffe58303e26a646f21b
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-05-23 18:14:39 -0500

    def xtract_text(file_path: str):

--- python/pdf_extract_text.py
+++ python/pdf_extract_text.py
@@ -1,21 +1,17 @@
 from PyPDF2 import PdfReader
 
-# Open the PDF file
-pdfFile = open('visitSummary.pdf', 'rb')
-
-# Create a PdfReader object to read the file
-pdfReader = PdfReader(pdfFile)
-
-# Get the number of pages in the PDF
-numOfPages = len(pdfReader.pages)
-
-# Loop through all the pages and extract text
-for i in range(0, numOfPages):
-    print("Page Number: " + str(i))
-    print("- - - - - - - - - - - - - - - - - - - -")
-    pageObj = pdfReader.pages[i]
-    print(pageObj.extract_text())
-    print("- - - - - - - - - - - - - - - - - - - -")
-
-# Close the PDF file object
-pdfFile.close()
+def xtract_text(file_path: str):
+    # Open the PDF file
+    pdfFile = open(file_path, 'rb')
+    # Create a PdfReader object to read the file
+    pdfReader = PdfReader(pdfFile)
+    # Get the number of pages in the PDF
+    numOfPages = len(pdfReader.pages)
+    # Loop through all the pages and extract text
+    text = ''
+    for i in range(0, numOfPages):
+        pageObj = pdfReader.pages[i]
+        text = text + pageObj.extract_text()
+    # Close the PDF file object
+    pdfFile.close()
+    return text
